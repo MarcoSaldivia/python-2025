e4a24a6e4ebce9130734c22a73156f8460c761ca
Marco Saldivia <marcoantonio.saldivia@alumnos.ulagos.cl>
2025-05-16 15:15:38 -0400

    Subiendo primeros ejemplos de variables

---
+++ 01-variables.py
@@ -0,0 +1,71 @@
+nombre = "Marco"
+apellido = "Saldivia"
+
+# UTILIZANDO VARIABLES EN UN PRINT CON COMAS
+print("Hola mi nombre es:", nombre, "y mi apellido es:", apellido )
+
+# IMPRIMIENDO CON OPERADOR DE CONCATENACION
+print("Hola mi nombre es: " + nombre + " y mi apellido es: " + apellido )
+
+# IMPRIMIENDO CON F-STRINGS(CADENAS LITERALES)
+print(f"Hola mi nombre es {nombre} y mi apellido es {apellido}")
+
+# INICIALIZANDO MULTI VARIABLES EN UNA SOLA LINEA (NO RECOMENDADO)
+ciudad, region, pais = "Castro", "Los Lagos", "Chile"
+print(f"Hola soy de {ciudad}, {region}, {pais}")
+
+# USO DEL INPUT / MUTABILIDAD
+print(nombre)
+nombre = input("Ingrese su nombre:")
+print(f"Hola mi nombre es: (nombre)") 
+
+print(nombre)
+
+
+
+
+estatura = 1.80
+peso = 105
+complejo = 2 + 9j # Inicializando un numero complejo
+complejo2 = complex(1, 4) # Otra forma de inicializar un numero complejo
+
+# IMPRESION DE NUMEROS COMPLEJOS
+print(complejo)
+print(complejo2)
+
+# OPERACION ARITMETICA
+imc = peso / (estatura ** 2)
+print(f"Su IMC es: {imc}")
+
+# FORMATO DE SALIDA DEL RESULTADO (UTILIZANDO EL METODO FORMAT)
+print("Su IMC es: {:,2f}", format(imc))
+
+# FORMATO DE SALIDA DEL RESULTADO (UTILIZANDO F-STRINGS Y :.2F)
+print(f"Su IMC es: {imc:.2f}") #Salida con 2 decimales
+
+
+
+
+# DATOS DE TIPO STRING (CADENAS DE TEXTO)
+carrera = 'Ingenieria Civil en Informatica'
+asignatura = "Programacion"
+descripcion = '''Esta es una asignatura de primer semestre'''
+
+# IMPRESION DE CARACTERES EN UNA CADENA DE TEXTO
+print(carrera[0]) # Imprime el primer caracter de la cadena
+print(carrera[-1]) # Imprime el ultimo caracter de la cadena
+
+
+
+
+print("Hola" * 4)
+# print("Hola"/2)
+
+# UTILIZANDO EL INTERVALO DE UNA CADENA
+print(asignatura[0:6])
+
+# APLICANDO EL METODO SPLIT (GENERA ARREGLO DE CADENAS)
+print(description.split())
+
+# ARREGLO NUMERICO
+v = [1, 2, 3, 4, 5] #
