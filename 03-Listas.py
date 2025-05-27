18179c73ba8c5c33e3c4cac186f13cff8388e486
Marco Saldivia <marcoantonio.saldivia@alumnos.ulagos.cl>
2025-05-27 19:10:05 -0400

    Actualizando ejemplos variables

---
+++ 03-Listas.py
@@ -0,0 +1,28 @@
+lista1 = ['Marco', 18, True]
+print(lista1)
+
+# LISTA DE NUMEROS
+n = [1,2.3.4.5]
+
+# LISTA DE SOLO STRINGS - UTILIZANDO LA FUNCION LIST
+ramos = list(['Programacion', 'Quimica', 'POO', 'Programacion' ])
+
+# IMPRIME LA POSICION DEL PRIMER ELEMENTO DE LA LISTA (NO EL CARACTER)
+print(ramos[0])
+
+# FUNCION COUNT (CUENTA LA CANTIDAD DE CONCURRENCIAS DE UN ELEMENTO)
+print(ramos.count('Programacion'))
+
+
+
+# CREANDO UNA TUPLA
+estudiantes = ('Samir', 'Hector', 'Matias', 'Pia', 'Carlos')
+
+# FUNCION INDEX EN TUPLAS
+print(estudiantes.index('Carlos'))
+
+
+
+# CREANDPO SETS
+colores = {'Azul', 'Rojo', 'Azul', 'Verde'}
+print(colores)
