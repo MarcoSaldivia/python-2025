75997d0a1ede6f6f96bd0e49cbcaf9694b5050f3
MarcoSaldivia <marcoantonio.saldivia@alumnos.ulagos.cl>
2025-05-30 14:26:16 -0400

    Actualizando ejemplos de listas

--- 03-Listas.py
+++ 03-Listas.py
@@ -2,10 +2,10 @@ lista1 = ['Marco', 18, True]
 print(lista1)
 
 # LISTA DE NUMEROS
-n = [1,2.3.4.5]
+n = [1,2,3,4,5]
 
 # LISTA DE SOLO STRINGS - UTILIZANDO LA FUNCION LIST
-ramos = list(['Programacion', 'Quimica', 'POO', 'Programacion' ])
+ramos = list(['Programacion', 'Quimica', 'POO', 'Programacion'])
 
 # IMPRIME LA POSICION DEL PRIMER ELEMENTO DE LA LISTA (NO EL CARACTER)
 print(ramos[0])
@@ -13,16 +13,33 @@ print(ramos[0])
 # FUNCION COUNT (CUENTA LA CANTIDAD DE CONCURRENCIAS DE UN ELEMENTO)
 print(ramos.count('Programacion'))
 
+# FUNCION APPEND (AGREGANDO UN ELEMENTO AL FINAL DE LA LISTA)
+ramos.append('Introduccion a la Matematica')
+print(ramos)
 
+# MODIFICAR ELEMENTOS A LA LISTA
+ramos[1] = "Comunicacion" # Estoy pasando la posicion del elemento modificado
+print(ramos)
 
-# CREANDO UNA TUPLA
-estudiantes = ('Samir', 'Hector', 'Matias', 'Pia', 'Carlos')
+# OTRA FORMA DE INSERTAR UN ELEMENTO A LA LISTA (INSERT)
+ramos.insert(0, 'Algebra')
 
-# FUNCION INDEX EN TUPLAS
-print(estudiantes.index('Carlos'))
+print(ramos)
 
+# ELIMINAR EL ULTIMO ELEMENTO DE LA LISTA
+ramos.pop()
+print(ramos)
 
+# ORDENANDO ELEMENTOS DE UNA LISTA 
+ramos.sort()
+print(ramos)
 
-# CREANDPO SETS
-colores = {'Azul', 'Rojo', 'Azul', 'Verde'}
-print(colores)
+# ORDENANDO ELEMENTOS DE UNA LISTA SEGUN SU CANTIDAD DE CARACTERES
+# Key es una propiedad del metodo sort y se pasa un valor que es el metodo len
+ramos.sort(key=len)
+print(ramos)
+
+# OCUPANDO EL METODO EXTEND (EXTENDIENDO UNA LISTA A PARTIR DE OTRA)
+ramitos = ['Calculo','Automatas'] # Nueva lista creada
+ramos.extend(ramitos)
+print(ramos)
